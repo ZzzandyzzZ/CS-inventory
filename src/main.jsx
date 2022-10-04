89b4bf96c1765b5c12a06dba336f3dc9bd01892e
ZzzandyzzZ <andy_gean@hotmail.com>
2022-10-04 15:50:27 -0500

    Use HashRouter instead BrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,15 @@ import ReactDOM from 'react-dom/client'
 import { CsInventoryApp } from './CsInventoryApp'
 import './index.css'
 import { AppTheme } from './theme/AppTheme'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <AppTheme>
         <CsInventoryApp />
       </AppTheme>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 )
