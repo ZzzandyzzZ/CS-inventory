04625d6e5512ca382504d8f5a86feec2d1af0e6a
ZzzandyzzZ <andy_gean@hotmail.com>
2022-10-04 13:00:28 -0500

    Create Apptheme

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { CsInventoryApp } from './CsInventoryApp'
 import './index.css'
+import { AppTheme } from './theme/AppTheme'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <CsInventoryApp />
+    <AppTheme>
+      <CsInventoryApp />
+    </AppTheme>
   </React.StrictMode>
 )
---
+++ src/theme/blueTheme.js
@@ -0,0 +1,7 @@
+import { createTheme } from '@mui/material';
+import { red } from '@mui/material/colors';
+
+export const blueTheme = createTheme({
+  palette:{
+  }
+})
