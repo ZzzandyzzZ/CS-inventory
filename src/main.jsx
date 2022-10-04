d2968bdf96911830676d6b0498b6886bb23087fa
ZzzandyzzZ <andy_gean@hotmail.com>
2022-10-04 14:32:00 -0500

    Add browser router

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client'
 import { CsInventoryApp } from './CsInventoryApp'
 import './index.css'
 import { AppTheme } from './theme/AppTheme'
+import { BrowserRouter } from 'react-router-dom'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AppTheme>
-      <CsInventoryApp />
-    </AppTheme>
+    <BrowserRouter>
+      <AppTheme>
+        <CsInventoryApp />
+      </AppTheme>
+    </BrowserRouter>
   </React.StrictMode>
 )
