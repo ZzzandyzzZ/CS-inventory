6d74019595de25642dbaa7d93f0309ca38d6546c
ZzzandyzzZ <andy_gean@hotmail.com>
2022-10-21 19:13:09 -0500

    Create constants to paths

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,1 +1,5 @@
 export const drawerWidth = 240;
+export const usersPathName = 'users';
+export const objectsPathName = 'objects';
+export const classroomPathName = 'classrooms';
+export const dashboardRootName = '/dashboard';
