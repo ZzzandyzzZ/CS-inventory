322b6f6d01e1bc4f3189537d134a9dc5b45aba05
ZzzandyzzZ <andy_gean@hotmail.com>
2022-10-21 19:14:43 -0500

    Create ViewLayout component

---
+++ src/models/dashboard/layouts/ViewLayout.jsx
@@ -0,0 +1,41 @@
+import Box from '@mui/material/Box';
+import Tab from '@mui/material/Tab';
+import TabContext from '@mui/lab/TabContext';
+import TabList from '@mui/lab/TabList';
+import TabPanel from '@mui/lab/TabPanel';
+import { useState } from 'react';
+import { Grid, Typography } from '@mui/material';
+
+export const ViewLayout = ({AddComponent, ListComponent, title, Icon}) => {
+  const [value, setValue] = useState('1');
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Box sx={{ width: '100%', typography: 'body1' }}>
+      <Grid container
+        justifyContent='center'
+        alignItems='center'
+      >
+        <Icon sx={{fontSize: '80px', pr: '10px'}} />
+        <Typography variant='h4'>{ title }</Typography>
+      </Grid>
+      <TabContext value={value}>
+        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+          <TabList onChange={handleChange} centered>
+            <Tab label="Agregar" value="1" />
+            <Tab label="Listar" value="2" />
+          </TabList>
+        </Box>
+        <TabPanel value="1">
+          <AddComponent />
+        </TabPanel>
+        <TabPanel value="2">
+          <ListComponent />
+        </TabPanel>
+      </TabContext>
+    </Box>
+  );
+}
