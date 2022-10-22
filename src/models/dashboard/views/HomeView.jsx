00c81806ae308350aeaf81e18c4a631ff032fd9c
ZzzandyzzZ <andy_gean@hotmail.com>
2022-10-21 19:15:48 -0500

    Create HomeView component

---
+++ src/models/dashboard/views/HomeView.jsx
@@ -0,0 +1,62 @@
+
+import { Grid } from '@mui/material';
+import { ResumeCard } from '../components/ResumeCard';
+
+import PeopleIcon from '@mui/icons-material/People';
+import ViewInArIcon from '@mui/icons-material/ViewInAr';
+import MapsHomeWorkIcon from '@mui/icons-material/MapsHomeWork';
+import { classroomPathName, objectsPathName, usersPathName } from '../../../constants/constants';
+
+const resumeCards = [
+  {}
+]
+
+export const HomeView = () => {
+  return (
+    <Grid container spacing={10}>
+      <ResumeCard Icon={PeopleIcon} title='Usuarios' totalNumber={4} pathName={usersPathName} />
+      <ResumeCard Icon={ViewInArIcon} title='Objetos' totalNumber={36} pathName={objectsPathName} />
+      <ResumeCard Icon={MapsHomeWorkIcon} title='Aulas' totalNumber={15} pathName={classroomPathName} />
+    </Grid>
+  )
+}
+
+// export const HomeView = () => {
+//   return (
+//     <Grid container spacing={3}>
+//       {/* Chart */}
+//       <Grid item xs={12} md={8} lg={9}>
+//         <Paper
+//           sx={{
+//             p: 2,
+//             display: 'flex',
+//             flexDirection: 'column',
+//             height: 240,
+//           }}
+//         >
+//           <Chart />
+//         </Paper>
+//       </Grid>
+//       {/* Recent Deposits */}
+//       <Grid item xs={12} md={4} lg={3}>
+//         <Paper
+//           sx={{
+//             p: 2,
+//             display: 'flex',
+//             flexDirection: 'column',
+//             height: 240,
+//           }}
+//         >
+//           <Deposits />
+//         </Paper>
+//       </Grid>
+//       {/* Recent Orders */}
+//       <Grid item xs={12}>
+//         <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column' }}>
+//           <Orders />
+//         </Paper>
+//       </Grid>
+//     </Grid>
+//   )
+// }
+
