8b180759c3e411b47d0b214bd7829cb3c1924eb5
ZzzandyzzZ <andy_gean@hotmail.com>
2022-10-21 19:15:19 -0500

    Update DashboardRoutes component

--- src/models/dashboard/routes/DashboardRoutes.jsx
+++ src/models/dashboard/routes/DashboardRoutes.jsx
@@ -1,10 +1,19 @@
-import { Route, Routes } from 'react-router-dom'
+import { Navigate, Route, Routes } from 'react-router-dom'
+import { classroomPathName, objectsPathName, usersPathName } from '../../../constants/constants'
 import { DashboardPage } from '../pages/DashboardPage'
+import { ClassroomView } from '../views/ClassroomView'
+import { HomeView } from '../views/HomeView'
+import { ObjectView } from '../views/ObjectView'
+import { UserView } from '../views/UserView'
 
 export const DashboardRoutes = () => {
   return (
     <Routes>
-      <Route path='*' element={<DashboardPage />}/>
+      <Route path={usersPathName} element={<DashboardPage View={UserView}/>}/>
+      <Route path={objectsPathName} element={<DashboardPage View={ObjectView}/>}/>
+      <Route path={classroomPathName} element={<DashboardPage View={ClassroomView}/>}/>
+      <Route path='/' element={<DashboardPage View={HomeView}/>}/>
+      {/* <Route path='/*' element={<Navigate to='/dashboard' />}/> */}
     </Routes>
   )
 }
