ce87ae9976530e133a5f0bf1fa23b281741043a3
Andy <andy@apurata.com>
2022-11-08 15:03:44 -0500

    Add backBaseURL

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -3,3 +3,4 @@ export const usersPathName = 'users';
 export const objectsPathName = 'objects';
 export const classroomPathName = 'classrooms';
 export const dashboardRootName = '/dashboard';
+export const backBaseURL = 'https://antszz.pythonanywhere.com/';
